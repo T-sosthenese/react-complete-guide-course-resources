60257c592a97b7727639f491808ae7c967d0f2e0
T-sosthenese <sosthenetimi94@gmail.com>
2025-02-08 11:56:54 +0300

    Conditional rendering

--- code/03 React Essentials/02-creating-using-first-component/src/App.jsx
+++ code/03 React Essentials/02-creating-using-first-component/src/App.jsx
@@ -33,11 +33,23 @@ function CoreConcept({ image, title, description }) {
 }
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
 
+  let tabContent = <h3>Please select a topic.</h3>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
@@ -60,13 +72,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
